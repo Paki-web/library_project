7daa2a28aef2b7a015fa7ab302f910be8d3bac17
Patryk Mlodawski <pk.mlodawski@gmail.com>
2020-01-20 22:58:42 +0100

    delete

--- includes/class-autoloader.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-spl_autoload_register('myAutoLoader');
-function myAutoLoader($className)
-{
-    $extension = ".class.php";
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/' . $className . $extension;
-}
